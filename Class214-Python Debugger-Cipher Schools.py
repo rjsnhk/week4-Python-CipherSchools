49bb9c7867766c451d066db39ae1be9b71013ca3
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-01-30 00:20:23 +0530

    Add files via upload

---
+++ Class214-Python Debugger-Cipher Schools.py
@@ -0,0 +1,7 @@
+import pdb
+pdb.set_trace()
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"hello {name} your age is {age}") 
+age2 = age+5
+print(f"hello {name} your age will be {age} in next five years") 
---
+++ Class216-With Block - Read File-Cipher Schools.py
@@ -0,0 +1,4 @@
+with open('file.txt') as f:
+    data = f.read()
+    print(data)
+print(f.closed)
